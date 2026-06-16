9a6d190a00687f4b19989b02d04edf73fa551bb0
Jair Silverio <jair.silverio999@gmail.com>
2026-06-16 01:16:51 -0600

    implementacion FileReader

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+//const e = require("express")
+
 const inputDoc = document.getElementById('inputDoc')
 const buttonSubmit = document.getElementById('buttonSubmit')
+const readerFile = new FileReader()
 
 buttonSubmit.addEventListener('click', function(){
-    console.log(inputDoc.files)
-    console.log(inputDoc.files[0])
-    console.log(inputDoc.files[0].name)
+    const file = inputDoc.files[0]
+    const data = readerFile.readAsArrayBuffer(file)
+    console.log(inputDoc.file)
+    console.log(file)
+    console.log(file.name)
     console.log(XLSX)
+    console.log(data)
 })
+
+readerFile.addEventListener('load', (e) => {
+    const data = e.target.result
+    console.log(data)
+})
+
+
+/*
+disculpa
+
+ */
